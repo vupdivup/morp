32b242b0eee36ef22138e1a724ac685607f59627
vupdivup <vnmatt2003@gmail.com>
2025-02-04 10:41:01 +0100

    reorganize timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { Profiler  } from "react";
-import { TimerWrapper } from "/src/components/Timer.jsx";
+import { Timer } from "/src/components/Timer.jsx";
 
 export function App() {
     return (
-        <Profiler id="app" onRender={() => console.log(Date.now())}>
-        <TimerWrapper></TimerWrapper>
-        </Profiler>
+        <Timer />
     );
 }
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,48 +1,53 @@
-import { useDeferredValue, useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
+import { ControlBar } from "./ControlBar";
 import { Digits } from "./Digits";
 import { ProgressBar } from "./ProgressBar";
-import { ControlBar } from "./ControlBar";
 import { Ring } from "./Ring";
 
-export function TimerWrapper() {
+const presets = [
+    {
+        name: "Focus",
+        duration: 3,
+        drawProgress: true,
+        color1: "#972d2d",
+        color2: "#641a1a",
+        sound: "break"
+    },
+    {
+        name: "Short Break",
+        duration: 3,
+        drawProgress: false,
+        color1: "#467638",
+        color2: "#205220",
+        sound: "focus"
+    },
+    {
+        name: "Long Break",
+        duration: 4,
+        drawProgress: false,
+        color1: "#3f5883",
+        color2: "#1d3964",
+        sound: "focus"
+    }
+];
+
+// ordered list of presets in cycle
+const queue = [0, 1, 0, 1, 0, 1, 0, 2];
+
+const sfx = {
+    focus: new Audio("/assets/sfx/focus.wav"),
+    break: new Audio("/assets/sfx/break.wav")
+};
+
+export function Timer() {
     const [state, setState] = useState("inactive");
 
     const [queueIdx, setQueueIdx] = useState(0);
 
     const [worker, setWorker] = useState(null);
 
-    const presets = [
-        {
-            name: "Focus",
-            duration: 3,
-            drawProgress: true,
-            color1: "#972d2d",
-            color2: "#641a1a",
-            sound: "break"
-        },
-        {
-            name: "Short Break",
-            duration: 3,
-            drawProgress: false,
-            color1: "#467638",
-            color2: "#205220",
-            sound: "focus"
-        },
-        {
-            name: "Long Break",
-            duration: 4,
-            drawProgress: false,
-            color1: "#3f5883",
-            color2: "#1d3964",
-            sound: "focus"
-        }
-    ];
-
-    // ordered list of presets in cycle
-    const queue = [0, 1, 0, 1, 0, 1, 0, 2];
-
-    const preset = presets[queue[queueIdx]]
+    const preset = presets[queue[queueIdx]];
 
     const [time, setTime] = useState(preset.duration);
 
@@ -69,11 +74,6 @@ export function TimerWrapper() {
         return () => {
             window.removeEventListener("keydown", handleKeydown);
         }
-    });
-
-    const sfx = useRef({
-        focus: new Audio("/assets/sfx/focus.wav"),
-        break: new Audio("/assets/sfx/break.wav")
     });
 
     const theme = { color1: preset.color1, color2: preset.color2 };
@@ -106,7 +106,7 @@ export function TimerWrapper() {
         const newTime = time - 1;
 
         if (newTime === 0) {
-            sfx.current[preset.sound].play();
+            sfx[preset.sound].play();
             shiftPreset(1);
         }
         else {
